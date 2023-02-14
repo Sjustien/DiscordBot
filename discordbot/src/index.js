1cfe04c08c3f6b870fa980377939677ba97e8932
Sjustien <97096053@st.deltion.nl>
2023-02-14 10:56:16 +0100

    First Commit

---
+++ discordbot/src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+import { Client, IntentsBitField } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log("wow");
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === 'hello') {
+    message.reply('hello');
+  }
+});
+
+client.login(process.env.TOKEN);
